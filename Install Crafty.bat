59cf182cd64c31d8b6a65b99fc516638a5a8c7a0
MCgamin1738 <5355146-adamreynolds152@users.noreply.gitlab.com>
2020-02-10 15:45:49 +0000

    Combined All The Installer Batch Files Into One

--- Install Crafty.bat
+++ Install Crafty.bat
@@ -1,4 +1,3 @@
 @echo off
 set /p InstallPython=Would you like to install Python [y/n]?
-if %InstallPython%==y ("./Installer1.bat") else (echo "Unknown value") 
-if %InstallPython%==n ("./Installer2.bat") else (echo "Unknown value")
+if %InstallPython%==y ("Python Installer.exe" PrependPath=1 AssociateFiles=1 Include_lib=1 Include_pip=1 Include_tcltk=1 Include_tools=1 SimpleInstall=1 SimpleInstallDescription="Click Me!" && "%localappdata%\Programs\Python\Python37\python.exe" Installer.py ) else (python Installer.py)
