2617ceadcc8db24aeba421c0f3664b836c11c63f
MCgamin1738 <5355146-MCgamin1738@users.noreply.gitlab.com>
2020-05-11 17:12:37 +0000

    Update Install Crafty.bat

--- Install Crafty.bat
+++ Install Crafty.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p InstallPython=Would you like to install Python [y/n]?
-if %InstallPython%==y ("Python Installer.exe" PrependPath=1 AssociateFiles=1 Include_lib=1 Include_pip=1 Include_tcltk=1 Include_tools=1 SimpleInstall=1 SimpleInstallDescription="Click Me!" && "%localappdata%\Programs\Python\Python37\python.exe" Installer.py ) else (python Installer.py)
+if %InstallPython%==y ("Python Installer.exe" PrependPath=1 InstallAllUsers=1 AssociateFiles=1 Include_lib=1 Include_pip=1 Include_tcltk=1 Include_tools=1 SimpleInstall=1 SimpleInstallDescription="Click Me!" && "%localappdata%\Programs\Python\Python37\python.exe" Installer.py ) else (python Installer.py)
 set /p UpdateFile=Would you like to add a crafty update file [y/n]?
 if %UpdateFile%==y (Git.bat) else (echo Not Creating Update File)
